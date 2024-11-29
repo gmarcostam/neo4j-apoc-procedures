558032d2d048099a4f6dd5861ffe7ef2880a012f
gmarcostam <92850018+gmarcostam@users.noreply.github.com>
2024-11-29 14:53:24 +0100

    Fixex 4256: Refactoring exception

--- extended/src/main/java/apoc/load/Jdbc.java
+++ extended/src/main/java/apoc/load/Jdbc.java
@@ -111,9 +111,8 @@ public class Jdbc {
                 throw sqle;
             }
         } catch (Exception e) {
-            logsErrorAndThrowsException(e, query, log);
+            throw logsErrorAndThrowsException(e, query, log);
         }
-        return Stream.empty();
     }
 
     @Procedure(mode = Mode.DBMS)
@@ -147,12 +146,11 @@ public class Jdbc {
                 throw sqle;
             }
         } catch (Exception e) {
-            logsErrorAndThrowsException(e, query, log);
+            throw logsErrorAndThrowsException(e, query, log);
         }
-        return Stream.empty();
     }
 
-    private static void logsErrorAndThrowsException(Exception e, String query, Log log) {
+    private static RuntimeException logsErrorAndThrowsException(Exception e, String query, Log log) {
         String errorMessage = "Cannot execute SQL statement `%s`.%nError:%n%s";
         String exceptionMsg = e.getMessage();
 
@@ -163,7 +161,7 @@ public class Jdbc {
 
         Exception ex = new Exception(exceptionMsg);
         log.error(String.format("Cannot execute SQL statement `%s`.%nError:%n%s", query, exceptionMsg), ex);
-        throw new RuntimeException(String.format(errorMessage, query, exceptionMsg, "Please download and copy the JDBC driver into $NEO4J_HOME/plugins, more details at https://neo4j-contrib.github.io/neo4j-apoc-procedures/#_load_jdbc_resources"), ex);
+        return new RuntimeException(String.format(errorMessage, query, exceptionMsg, "Please download and copy the JDBC driver into $NEO4J_HOME/plugins, more details at https://neo4j-contrib.github.io/neo4j-apoc-procedures/#_load_jdbc_resources"), ex);
     }
 
     static void closeIt(Log log, AutoCloseable...closeables) {
